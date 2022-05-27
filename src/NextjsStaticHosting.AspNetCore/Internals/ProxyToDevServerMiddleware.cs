b778f566c32690c8013434294b74728e473d8c6e
David <david.nissimoff@gmail.com>
2022-05-27 12:59:49 -0300

    Emit useful error when unable to reach dev server in dev mode

--- src/NextjsStaticHosting.AspNetCore/Internals/ProxyToDevServerMiddleware.cs
+++ src/NextjsStaticHosting.AspNetCore/Internals/ProxyToDevServerMiddleware.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Yarp.ReverseProxy.Forwarder;
 
@@ -13,13 +14,15 @@ namespace NextjsStaticHosting.AspNetCore.Internals
         private readonly NextjsStaticHostingOptions options;
         private readonly IHttpForwarder yarpForwarder;
         private readonly RequestDelegate next;
+        private readonly ILogger<ProxyToDevServerMiddleware> logger;
         private readonly HttpMessageInvoker httpClient;
 
-        public ProxyToDevServerMiddleware(IOptions<NextjsStaticHostingOptions> options, IHttpForwarder yarpForwarder, RequestDelegate next)
+        public ProxyToDevServerMiddleware(IOptions<NextjsStaticHostingOptions> options, IHttpForwarder yarpForwarder, RequestDelegate next, ILogger<ProxyToDevServerMiddleware> logger)
         {
             this.options = options?.Value ?? throw new ArgumentNullException(nameof(options));
             this.yarpForwarder = yarpForwarder ?? throw new ArgumentNullException(nameof(yarpForwarder));
             this.next = next ?? throw new ArgumentNullException(nameof(next));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
 
             if (!this.options.ProxyToDevServer ||
                 string.IsNullOrEmpty(this.options.DevServer))
@@ -38,7 +41,20 @@ namespace NextjsStaticHosting.AspNetCore.Internals
 
         public async Task InvokeAsync(HttpContext context)
         {
-            await this.yarpForwarder.SendAsync(context, this.options.DevServer, this.httpClient, new ForwarderRequestConfig { ActivityTimeout = TimeSpan.FromMinutes(5) });
+            var error = await this.yarpForwarder.SendAsync(context, this.options.DevServer, this.httpClient, new ForwarderRequestConfig { ActivityTimeout = TimeSpan.FromMinutes(5) });
+            if (error != ForwarderError.None)
+            {
+                string message =
+                    $"[NextjsStaticHosting.AspNetCore] Unable to reach Next.js dev server. Please ensure it is running at {this.options.DevServer}.{Environment.NewLine}" +
+                    $"If you are running in production and did not intend to proxy to a Next.js dev server, please ensure {nameof(NextjsStaticHostingOptions)}.{nameof(NextjsStaticHostingOptions.ProxyToDevServer)} is false.{Environment.NewLine}" +
+                    $"YARP error: {error}";
+                this.logger.LogError(message);
+                if (!context.Response.HasStarted)
+                {
+                    context.Response.ContentType = "text/plain";
+                    await context.Response.WriteAsync(message);
+                }
+            }
         }
     }
 }
